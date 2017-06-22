44c3efda880546c5a70accaba718ee874ac2bb91
theleastinterestingcoder <quanzhou64@gmail.com>
2017-06-21 22:19:14 -0400

    adding routing for a landing page

--- FrontEnd_Injection/urls.py
+++ FrontEnd_Injection/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from injection import views
 
 urlpatterns = [
     url(r'^injection/', include('injection.urls')),
+    url('^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
 ]
